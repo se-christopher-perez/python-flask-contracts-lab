bfbbf9083356b7d5bbff8d0d64dd3434327fca81
Christopher Perez <Data.Christopher.Perez@Gmail.com>
2026-06-10 22:36:29 -0400

    update

--- server/app.py
+++ server/app.py
@@ -6,5 +6,22 @@ contracts = [{"id": 1, "contract_information": "This contract is for John and bu
 customers = ["bob","bill","john","sarah"]
 app = Flask(__name__)
 
+@app.route("/contract/<int:id>")
+def contract(id):
+    found = [contract for contract in contracts if contract["id"] == id]
+
+    if found:
+        return found[0]["contract_information"], 200
+    
+    return "Contract not found", 404
+
+@app.route("/customer/<string:customer_name>")
+def customer(customer_name):
+    found = [name for name in customers if name == customer_name]
+
+    if found:
+        return found[0], 204
+    return "Customer not found", 404
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
